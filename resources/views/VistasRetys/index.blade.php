3d74a8206658ac248a19ebfb6b0d0e87c43c6b5b
angelwayne <39891254+angelwayne@users.noreply.github.com>
2020-08-18 12:52:10 -0500

    lol

--- resources/views/VistasRetys/index.blade.php
+++ resources/views/VistasRetys/index.blade.php
@@ -3,204 +3,217 @@
 
 
 
-				<div class="btn-group mr-2" role="group" aria-label="First group">
-					
-					<button type="button" class="btn">
+			<div class="col-6 col-xl-3 col-md-4">
+				<a href="" title="Cana">
 						<div class="card border-0 bg-transparent responsive-categorias card-body">
 							<div class="responsive-categorias">
 								<img
 									src="img/icon/cana.png"
-									class="img">
+									class="img-fluid" width="70">
 								<p class="font-weight-bold text-special-color-dark align-self-center responsive-text-categorias">Adultos Mayores</p>
 							</div>
 						</div>
-					</button>
+				</a>
+			</div>
+
 					
-					<button type="button" class="btn">
+			<div class="col-6 col-xl-3 col-md-4">
+				<a href="" title="Educacion">
 						<div class="card border-0 bg-transparent responsive-categorias card-body">
 							<div class="responsive-categorias">
 								<img
 									src="img/icon/ICONOS_EDUCACION 2.png"
-									class="img">
+									class="img-fluid" width="70">
 								<p class="font-weight-bold text-special-color-dark align-self-center responsive-text-categorias">Educación</p>
 							</div>
 						</div>
-					</button>
-					
-					<button type="button" class="btn">
+				</a>
+			</div>
+			
+			
+			<div class="col-6 col-xl-3 col-md-4">
+				<a href="" title="Cultura">
+					<div class="col-6 col-xl-3 col-md-4">
+				<a href="" title="Cultura">
 						<div class="card border-0 bg-transparent responsive-categorias card-body">
 							<div class="responsive-categorias">
 								<img
 									src="img/icon/ICONOS_CULTURA 2.png"
-									class="img">
+									class="img-fluid" width="70">
 								<p class="font-weight-bold text-special-color-dark align-self-center responsive-text-categorias">Cultura </p>
 							</div>
 						</div>
-					</button>
+				</a>
+			</div>
 
-					<button type="button" class="btn">
+			<div class="col-6 col-xl-3 col-md-4">
+				<a href="" title="Cultura">
 						<div class="card border-0 bg-transparent responsive-categorias card-body">
 							<div class="responsive-categorias">
 								<img
 									src="img/icon/ICONOS_ECONOMIA Y NEGOCIO 2.png"
-									class="img" >
+									class="img-fluid" width="70" >
 								<p class="font-weight-bold text-special-color-dark align-self-center responsive-text-categorias">Finanzas</p>
 							</div>
 						</div>
-					</button>
+				</a>
+			</div>
+				
 
-				</div>
-
-				<br>
-
-				<div class="btn-group mr-2" role="group" aria-label="Second group">
-
-					<button type="button" class="btn">
+			<div class="col-6 col-xl-3 col-md-4">
+				<a href="" title="Cultura">
 						<div class="card border-0 bg-transparent responsive-categorias card-body">
 							<div class="responsive-categorias">
 								<img
 									src="img/icon/reclutamiento2.png"
-									class="img" >
+									class="img-fluid" width="70" >
 								<p
 									class="font-weight-bold text-special-color-dark align-self-center responsive-text-categorias">Empleo</p>
 							</div>
 						</div>
-					</button>
+				</a>
+			</div>
 
-					<button type="button" class="btn">
+			<div class="col-6 col-xl-3 col-md-4">
+				<a href="" title="Cultura">
 						<div class="card border-0 bg-transparent responsive-categorias card-body">
 							<div class="responsive-categorias">
 								<img
 									src="img/icon/ICONOS_FAMILIA CIUDADANIA .png"
-									class="img" >
+									class="img-fluid" width="70" >
 								<p
 									class="font-weight-bold text-special-color-dark align-self-center responsive-text-categorias">Familia</p>
 							</div>
 						</div>
-					</button>
+				</a>
+			</div>
 
-					<button type="button" class="btn">
+			<div class="col-6 col-xl-3 col-md-4">
+				<a href="" title="Cultura">
 						<div class="card border-0 bg-transparent responsive-categorias card-body">
 							<div class="responsive-categorias">
 								<img
 									src="img/icon/ICONOS_JURIDICO .png"
-									class="img" >
+									class="img-fluid" width="70" >
 								<p
 									class="font-weight-bold text-special-color-dark align-self-center responsive-text-categorias">Justicia</p>
 							</div>
 						</div>
-					</button>
+				</a>
+			</div>
 
-					<button type="button" class="btn">
+			<div class="col-6 col-xl-3 col-md-4">
+				<a href="" title="Cultura">
 						<div class="card border-0 bg-transparent responsive-categorias card-body">
 							<div class="responsive-categorias">
 								<img
 									src="img/icon/ICONOS_MEDIO AMBIENTE G-45.png"
-									class="img" >
+									class="img-fluid" width="70" >
 								<p
 									class="font-weight-bold text-special-color-dark align-self-center responsive-text-categorias">Medio Ambiente</p>
 							</div>
 						</div>
-					</button>
-
-				</div>
+				</a>
+			</div>
+		
 				
-					
-					
-				<div class="btn-group mr-2" role="group" aria-label="Third group">
-					
-					<button type="button" class="btn">
+			<div class="col-6 col-xl-3 col-md-4">
+				<a href="" title="Cultura">
 						<div class="card border-0 bg-transparent responsive-categorias card-body">
 							<div class="responsive-categorias">
 								<img
 									src="img/icon/ICONOS_PAGOS IMPUESTOS 2.png"
-									class="img" >
+									class="img-fluid" width="70" >
 								<p
 									class="font-weight-bold text-special-color-dark align-self-center responsive-text-categorias">Pago de Impuestos</p>
 							</div>
 						</div>
-					</button>
+				</a>
+			</div>
 
-					<button type="button" class="btn">
+			<div class="col-6 col-xl-3 col-md-4">
+				<a href="" title="Cultura">
 						<div class="card border-0 bg-transparent responsive-categorias card-body">
 							<div class="responsive-categorias">
 								<img
 									src="img/icon/ICONOS_QUEJA DENUNCIA 2.png"
-									class="img" >
+									class="img-fluid" width="70" >
 								<p
 									class="font-weight-bold text-special-color-dark align-self-center responsive-text-categorias">Quejas o Denuncias</p>
 							</div>
 						</div>
-					</button>
+				</a>
+			</div>
 
-					<button type="button" class="btn">
+			<div class="col-6 col-xl-3 col-md-4">
+				<a href="" title="Cultura">
 						<div class="card border-0 bg-transparent responsive-categorias card-body">
 							<div class="responsive-categorias">
 								<img
 									src="img/icon/ICONOS_SALUD 2.png"
-									class="img" >
+									class="img-fluid" width="70" >
 								<p
 								>Salud y Deporte</p>
 							</div>
 						</div>
-					</button>
+				</a>
+			</div>
 
-					<button type="button" class="btn btn">
+			<div class="col-6 col-xl-3 col-md-4">
+				<a href="" title="Cultura">
 						<div class="card border-0 bg-transparent responsive-categorias card-body">
 							<div class="responsive-categorias">
 								<img
 									src="img/icon/ICONOS_SEGURIDAD 2.png"
-									class="img" >
+									class="img-fluid" width="70" >
 								<p
 									class="font-weight-bold text-special-color-dark align-self-center responsive-text-categorias">Seguridad</p>
 							</div>
 						</div>
-					</button>
+				</a>
+			</div>
 				
-				</div>	
-
-				<div class="btn-group mr-2" role="group" aria-label="Fourd group">
 				
-					<button type="button" class="btn">
+			<div class="col-6 col-xl-3 col-md-4">
+				<a href="" title="Cultura">
 						<div class="card border-0 bg-transparent responsive-categorias card-body">
 							<div class="responsive-categorias">
 								<img
 									src="img/icon/ICONOS_TRANSPORTE G.png"
-									class="img" >
+									class="img-fluid" width="70" >
 								<p
 									class="font-weight-bold text-special-color-dark align-self-center responsive-text-categorias">Transporte y Urbanismo</p>
 							</div>
 						</div>
-					</button>
+				</a>
+			</div>
 
-					<button type="button" class="btn">
+			<div class="col-6 col-xl-3 col-md-4">
+				<a href="" title="Cultura">
 						<div class="card border-0 bg-transparent responsive-categorias card-body">
 							<div class="responsive-categorias">
 								<img
 									src="img/icon/ICONOS_TURISMO .png"
-									class="img" >
+									class="img-fluid" width="70" >
 								<p
 									class="font-weight-bold text-special-color-dark align-self-center responsive-text-categorias">Turismo</p>
 							</div>
 						</div>
-					</button>
+						</a>
+			</div>
 
-					<button type="button" class="btn">
+			<div class="col-6 col-xl-3 col-md-4">
+				<a href="" title="Cultura">
 						<div class="card border-0 bg-transparent responsive-categorias card-body">
 							<div class="responsive-categorias">
 								<img
 									src="img/icon/casa2.png"
-									class="img" >
+									class="img-fluid" width="70" >
 								<p
 									class="font-weight-bold text-special-color-dark align-self-center responsive-text-categorias">Vivienda</p>
 							</div>
 						</div>
-					</button>
-					
-					
-				
-				</div>
-
-		
+				</a>
+			</div>
 
 @stop
