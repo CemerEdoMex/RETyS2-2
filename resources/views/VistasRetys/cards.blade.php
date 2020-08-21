cb1572a946428ae4edf3bab00cbd035df28a52cd
angelwayne <al221311480@gmail.com>
2020-08-21 11:24:02 -0500

    Seccion de Trmites mas buscados
    Tarjetas para tramites mas buscados.

--- resources/views/VistasRetys/cards.blade.php
+++ resources/views/VistasRetys/cards.blade.php
@@ -1,37 +1,34 @@
 <link rel="stylesheet" href="{{ asset('vendor/cards.css') }}">
-
-<div class="container-fluid">
-<div class="wrap">
-    <h3> Tramites mas buscados 🔎</h3>
+	<div class="wrap">
+        <h2 class="tittleCard"> Tramites mas buscados...</h2>
 		<div class="tarjeta-wrap">
 			<div class="tarjeta">
-				<div class="adelante card1"></div>
+				<div class="adelante card1">
+				  	  <h4 class="tittleCard"> Tramite Nombre </h4>
+				</div>
 				<div class="atras">
-                    <h2>Tramite 1</h2>
-                    <hr>
 					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa ex velit beatae. Illum, suscipit, aspernatur!</p>
 				</div>
 			</div>
 		</div>
 		<div class="tarjeta-wrap">
 			<div class="tarjeta">
-				<div class="adelante card2"></div>
+				<div class="adelante card2">
+				  	  <h4 class="tittleCard"> Tramite Nombre </h4>
+				</div>
 				<div class="atras">
-                    <h2>Tramite 2</h2>
-                    <hr>
 					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa ex velit beatae. Illum, suscipit, aspernatur!</p>
 				</div>
 			</div>
 		</div>
 		<div class="tarjeta-wrap">
 			<div class="tarjeta">
-				<div class="adelante card3"></div>
+				<div class="adelante card3">
+				  <h4 class="tittleCard"> Tramite Nombre </h4>
+				</div>
 				<div class="atras">
-                    <h2>Tramite 3</h2>
 					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa ex velit beatae. Illum, suscipit, aspernatur!</p>
 				</div>
 			</div>
 		</div>
 	</div>
-
-</div>
